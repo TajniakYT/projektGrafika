177b33f8464214f4ee760befeef1f63951ea0877
MichalWinkler <115418478+MichalWinkler@users.noreply.github.com>
2025-06-02 21:53:00 +0200

    poprawki
    Zmiana koloru tekstu w UI poziomu, zmiana koloru pociskow, schowanie narazie nieuzywanych assetow, skrypt rotation do odpowiedniego foldera, skrypt rotation dodany do enemyHeli, zycie gracza ustawione na normalna wartosc

--- Assets/Scripts/Player/ObjectiveText.cs
+++ Assets/Scripts/Player/ObjectiveText.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ObjectiveText : MonoBehaviour
 {
     public TMP_Text objectiveText;
-    string objective;
+    string objective = "destroy all enemies";
 
     // Start is called before the first frame update
     void Start()
